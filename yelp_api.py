b510cf2eacdc98a25f7e63e9e5552eacf403ab70
Cristina Clarkin <cristinaclarkin@Cristina--JJs-MacBook-Pro.local>
2016-05-10 16:23:31 -0700

    first day work

---
+++ yelp_api.py
@@ -0,0 +1,21 @@
+import os
+from yelpapi import YelpAPI
+import pprint
+
+
+
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+TOKEN = os.environ['TOKEN']
+TOKEN_SECRET = os.environ['TOKEN_SECRET']
+
+
+
+location = "location=San+Francisco,CA,USA"
+term = "term=tacos"
+limit = 1
+
+yelp = YelpAPI(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET)
+search_results = yelp_api.search_query(location=location, term=term, limit=limit)
+printer = pprint.PrettyPrinter()
+printer.pprint(search_results)
